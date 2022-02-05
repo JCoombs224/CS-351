f3acec7fdec62d67fe6c803c7d891c44bc42d7f0
Jamison <jamiedude224@gmail.com>
2022-02-05 13:46:14 -0800

    Notes now play from the music box

--- Assignment 1 Music Box/script.js
+++ Assignment 1 Music Box/script.js
@@ -4,14 +4,62 @@ var html_template = "<html><body><script>"
 function play_notes(){
     song = document.getElementById("textBox").value;
     console.log(song);
-    lines = song.split("\r?\n");
+    lines = song.split(/\r?\n/);
+    console.log(lines);
     sound_delay = 0;
     compiled_script = "";
     
     for(i = 0; i < lines.length; ++i)
     {
         console.log(lines[i]);
+        sound_delay = (1000 * i);
+        if(lines[i][0] == 'E')
+        {
+            console.log("Play E")
+            setTimeout(function() {
+                new Audio('sounds/E.wav').play();
+            }, sound_delay);
+        }
+        if(lines[i][1] == 'A')
+        {
+            console.log("Play A")
+            setTimeout(function() {
+                new Audio('sounds/A.wav').play();
+            }, sound_delay);
+        }
+        if(lines[i][2] == 'D')
+        {
+            console.log("Play D")
+            setTimeout(function() {
+                new Audio('sounds/D.wav').play();
+            }, sound_delay);
+        }
+        if(lines[i][3] == 'G')
+        {
+            console.log("Play G")
+            setTimeout(function() {
+                new Audio('sounds/G.wav').play();
+            }, sound_delay);
+        }
+        if(lines[i][4] == 'F')
+        {
+            console.log("Play F")
+            setTimeout(function() {
+                new Audio('sounds/F.wav').play();
+            }, sound_delay);
+        }
+        if(lines[i][5] == 'C')
+        {
+            console.log("Play C")
+            setTimeout(function() {
+                new Audio('sounds/C.wav').play();
+            }, sound_delay);
+        }
     }
 
     document.getElementById("compiled")
+}
+
+function clear_music(){
+    document.getElementById("textBox").value = "";
 }
